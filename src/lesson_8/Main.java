a19bc8486d0008753a371c171ddee664587a72c5
Story81 <story81@bk.ru>
2024-02-24 22:13:06 +0400

    Добавлен урок 9

--- src/lesson_8/Main.java
+++ src/lesson_8/Main.java
@@ -4,11 +4,25 @@ package lesson_8;
 public class Main {
     public static void main(String[] args) {
 
-        Rectangle rectangle1 = new Rectangle("Треугольник",3,9,11);
-        Rectangle rectangle2 = new Rectangle("Треугольник",3,3,3);
+        Triangle triangle1 = new Triangle("Треугольник",3,9,11);
+        Triangle triangle2 = new Triangle("Треугольник",3,9,11);
         Square square = new Square("Квадрат", 4);
+        Rectangle rectangle = new Rectangle("Прямоугольник", 3, 8);
 
-        rectangle1.perimeter();
+
+        System.out.println("Дано: ");
+        System.out.println(triangle1);
+        System.out.println(square);
+        System.out.println(rectangle);
+
+        System.out.println("\nВычисления: ");
+        triangle1.perimeter();
         square.perimeter();
+        rectangle.perimeter();
+
+        System.out.println("\nРавенства: ");
+        System.out.println("triangle1 равен triangle2? - " +  triangle1.equals(triangle2));
+        System.out.println("square равен triangle2? - " +  square.equals(triangle2));
+
     }
 }
--- src/lesson_8/Square.java
+++ src/lesson_8/Square.java
@@ -1,6 +1,16 @@
 package lesson_8;
 
 public class Square extends Figure{
+   private int side1;
+
+    public int getSide1() {
+        return side1;
+    }
+
+    public void setSide1(int side1) {
+        this.side1 = side1;
+    }
+
     @Override
     public  void perimeter(){
         perimeter = side1*4;
@@ -11,4 +21,25 @@ public class Square extends Figure{
         this.figureName = figureName;
         this.side1 = side1;
           }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Square square = (Square) o;
+
+        return side1 == square.side1;
+    }
+
+    @Override
+    public int hashCode() {
+        return side1;
+    }
+
+    @Override
+    public String toString() {
+        return "Квадрат со стороной " +
+                "а = " + side1;
+    }
 }
