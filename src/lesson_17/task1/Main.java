3af0c5c2af5d0a9aedf4f423bc65d7d608d8eedb
Story81 <story81@bk.ru>
2024-04-06 16:57:53 +0400

    урок 17

---
+++ src/lesson_17/task1/Main.java
@@ -0,0 +1,23 @@
+package lesson_17.task1;
+
+public class Main {
+    public static void main(String[] args) {
+//        ICopyUtils ICopyUtils = new CopyWithIO();
+//        long start = System.currentTimeMillis();
+//        ICopyUtils.copy("C:\\Users\\CASPER\\Desktop\\1022-Соломенникова-Надежда.jpg", "1.jpg");
+//        long end = System.currentTimeMillis();
+//        System.out.println("Время копирования с помощью метода IO = " + (end-start));
+//
+//        ICopyUtils ICopyUtils = new CopyWithNIO();
+//        long start = System.currentTimeMillis();
+//        ICopyUtils.copy("C:\\Users\\CASPER\\Desktop\\1022-Соломенникова-Надежда.jpg", "1.jpg");
+//        long end = System.currentTimeMillis();
+//        System.out.println("Время копирования с помощью метода NIO = " + (end-start));
+
+        ICopyUtils ICopyUtils = new CopyWitApache();
+        long start = System.currentTimeMillis();
+        ICopyUtils.copy("C:\\Users\\CASPER\\Desktop\\1022-Соломенникова-Надежда.jpg", "1.jpg");
+        long end = System.currentTimeMillis();
+        System.out.println("Время копирования с помощью метода Apache = " + (end-start));
+    }
+}
